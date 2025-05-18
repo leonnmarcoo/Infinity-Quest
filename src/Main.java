f71bc416f8fd36cfb3c27cb6fa2970418fdd0cbc
leonnmarcoo <develaleonmarco@gmail.com>
2025-05-18 19:44:18 +0800

    Added AdminLogin

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("Scene1.fxml"));
-        primaryStage.setTitle("Hello Worlds");
-        primaryStage.setScene(new Scene(root, 400, 300));
+        Parent root = FXMLLoader.load(getClass().getResource("/Admin/FXML/AdminLogin.fxml"));
+        primaryStage.setTitle("Avengers");
+        primaryStage.setScene(new Scene(root, 1512, 982));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
