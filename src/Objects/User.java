1a2fc40f5a91b520eb607b0ce3045488f9cdbb89
John Mikael <147248782+Kyojurouu@users.noreply.github.com>
2025-05-21 00:13:01 +0800

    Updated ADMIN-USER Side (Mikey)
    Added features, Clearform, and more (i forgot)

--- src/Objects/User.java
+++ src/Objects/User.java
@@ -1,19 +1,26 @@
 package Objects;
 
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
 public class User {
     
+    private final SimpleIntegerProperty userID;
     private final SimpleStringProperty userName;
     private final SimpleStringProperty userPassword;
     private final SimpleStringProperty userEmail;
     private final SimpleStringProperty userBio;
 
-        public User(String userName, String userPassword, String userEmail, String userBio) {
+        public User(int userId,String userName, String userPassword, String userEmail, String userBio) {
+        this.userID = new SimpleIntegerProperty(userId);
         this.userName = new SimpleStringProperty(userName);
         this.userPassword = new SimpleStringProperty(userPassword);
         this.userEmail = new SimpleStringProperty(userEmail);
         this.userBio = new SimpleStringProperty(userBio);
+    }
+
+    public int getUserID() {
+        return userID.get();
     }
 
     public String getUserName() {
