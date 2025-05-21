a698292df1087b2127593f606fb6fd77d86a5ead
John Mikael <147248782+Kyojurouu@users.noreply.github.com>
2025-05-21 14:25:39 +0800

    Added AdminContentController BETA
    Created foundation for AdminContentController such as Objects, fixed fx:id for tables, added few queries for DatabaseHandler, and fixed/updated the AdminHomeController fxml file

--- src/Admin/Controller/AdminContentController.java
+++ src/Admin/Controller/AdminContentController.java
@@ -1,5 +1,129 @@
 package Admin.Controller;
 
+import Database.DatabaseHandler;
+import Objects.User;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TabPane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javax.naming.spi.DirStateFactory;
+import javax.xml.transform.Templates;
+
+
 public class AdminContentController {
-    
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button castButton;
+
+    @FXML
+    private Button contentButton;
+
+    @FXML
+    private TabPane contentTabPane;
+
+    @FXML
+    private Button createButton;
+
+    @FXML
+    private Button deleteButton;
+
+    @FXML
+    private Button dislikeButton;
+
+    @FXML
+    private Button likeButton;
+
+    @FXML
+    private Label mRuntimeLabel;
+
+    @FXML
+    private Label mTitleLabel;
+
+    @FXML
+    private TextField mTitleTextField;
+
+    @FXML
+    private Tab movieTab;
+
+    @FXML
+    private Button ratingButton;
+
+    @FXML
+    private Button reviewButton;
+
+    @FXML
+    private Button updateButton;
+
+    @FXML
+    private Button userButton;
+
+    @FXML
+    private Label userLabel;
+
+    @FXML
+    private Button watchlistButton;
+
+    private Stage stage;
+
+    private Scene scene;
+
+    private Parent root;
+
+
+    @FXML
+    private void userButtonHandler(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Admin/FXML/AdminUser.fxml"));
+            root = loader.load();
+            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void backButtonHandler(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(getClass().getResource("/Admin/FXML/AdminHome.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/Admin/Controller/AdminUserController.java
+++ src/Admin/Controller/AdminUserController.java
@@ -215,6 +215,22 @@ public class AdminUserController implements Initializable {
     }
 
     @FXML
+    private void contentButtonHandler(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Admin/FXML/AdminContent.fxml"));
+
+            root = loader.load();
+
+            stage = (Stage) ((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
     private void createUser(ActionEvent event) throws IOException {
 
         String username = usernameTextField.getText();
