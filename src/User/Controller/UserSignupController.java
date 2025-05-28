9e5216c00aa8951598b14c473dd27434a1f94d66
John Mikael <147248782+Kyojurouu@users.noreply.github.com>
2025-05-28 14:35:33 +0800

    USER-SIDE Sign-up RC
    Added Sign-up functions, Users now can sign up to our system.
    
    In coding this, I re-used some of the contents and codes from the admin side. (Objects - User, Database - createUser)

---
+++ src/User/Controller/UserSignupController.java
@@ -0,0 +1,105 @@
+package User.Controller;
+
+import Objects.User;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
+
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Hyperlink;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javax.naming.spi.DirStateFactory;
+import javax.xml.transform.Templates;
+
+import Database.DatabaseHandler;
+
+public class UserSignupController {
+
+    private User selectedUser = null;
+
+    @FXML
+    private TextField bioTextField;
+
+    @FXML
+    private TextField emailTextField;
+
+    @FXML
+    private PasswordField passwordPasswordField;
+
+    @FXML
+    private Button signUpButton;
+
+    @FXML
+    private TextField usernameTextField;
+
+    private void clearForm() {
+        usernameTextField.clear();
+        passwordPasswordField.clear();
+        emailTextField.clear();
+        bioTextField.clear();
+        selectedUser = null;
+    }    
+
+
+    @FXML
+    private void signUpButtonHandler(ActionEvent event) {
+        String username = usernameTextField.getText();
+        String email = emailTextField.getText();
+        String password = passwordPasswordField.getText();
+        String bio = bioTextField.getText();
+
+        User user = new User(0, username, password, email, bio);
+
+        if (username.isEmpty() || password.isEmpty() || email.isEmpty() || bio.isEmpty()) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText("Please fill in all fields");
+            alert.showAndWait();
+            return;
+        }
+
+        if (DatabaseHandler.createUser(user)) {
+            Alert alert = new Alert(AlertType.INFORMATION);
+            alert.setTitle("Success");
+            alert.setHeaderText(null);
+            alert.setContentText("User created successfully!");
+            alert.showAndWait();
+
+            clearForm();
+
+        } else {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Error");
+            alert.setHeaderText(null);
+            alert.setContentText("Failed to create an account.");
+            alert.showAndWait();
+        }
+    }
+
+}
