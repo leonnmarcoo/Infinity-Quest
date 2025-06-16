00895dbdd73de03de0493dca6d8b854c6540c72e
leonnmarcoo <develaleonmarco@gmail.com>
2025-06-16 21:11:50 +0800

    Bug fixes

--- src/User/Controller/UserLoginController.java
+++ src/User/Controller/UserLoginController.java
@@ -64,44 +64,42 @@ public class UserLoginController {
 
     public void loginButtonHandler(ActionEvent event) throws IOException {
 
-    String username = usernameTextField.getText();
-    String password = passwordPasswordField.getText();
+        String username = usernameTextField.getText();
+        String password = passwordPasswordField.getText();
 
-    if (DatabaseHandler.validateUserLogin(username, password)) {
+        if (DatabaseHandler.validateUserLogin(username, password)) {
 
-        User user = DatabaseHandler.getUserByUsername(username);
-        user.setWatched(DatabaseHandler.getWatchedTitles(user.getUserID()));
-        user.setWatchlist(DatabaseHandler.getWatchlistTitles(user.getUserID()));
-        user.setReviews(DatabaseHandler.getReviews(user.getUserID()));
+            User user = DatabaseHandler.getUserByUsername(username);
+            user.setWatched(DatabaseHandler.getWatchedTitles(user.getUserID()));
+            user.setWatchlist(DatabaseHandler.getWatchlistTitles(user.getUserID()));
+            user.setReviews(DatabaseHandler.getReviews(user.getUserID()));
 
-        SessionManager.setCurrentUser(user);
+            SessionManager.setCurrentUser(user);
 
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/User/FXML/UserHome.fxml"));
-        root = loader.load();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/User/FXML/UserHome.fxml"));
+            root = loader.load();
 
-        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-    }
-
-    User user = DatabaseHandler.getUserByUsername(username);
-    if (user == null) {
-        Alert alert = new Alert(AlertType.ERROR);
-        alert.setTitle("Login Failed");
-        alert.setHeaderText(null);
-        alert.setContentText("User not found");
-        alert.showAndWait();
-        return;
+            stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            return;
         }
 
-        else {
-        Alert alert = new Alert(AlertType.ERROR);
-        alert.setTitle("Login Failed");
-        alert.setHeaderText(null);
-        alert.setContentText("Credentials don't match.");
-        alert.showAndWait();
-     }
+        User user = DatabaseHandler.getUserByUsername(username);
+        if (user == null) {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Login Failed");
+            alert.setHeaderText(null);
+            alert.setContentText("User not found");
+            alert.showAndWait();
+        } else {
+            Alert alert = new Alert(AlertType.ERROR);
+            alert.setTitle("Login Failed");
+            alert.setHeaderText(null);
+            alert.setContentText("Invalid username or password.");
+            alert.showAndWait();
+        }
     }
 
 }
